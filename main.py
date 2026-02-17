7cc4ee5a143acb10efc26ff877209ee222a15913
T-Raheeq Qassrawi <t-raheeq.qassrawi@exalt.ps>
2026-02-17 13:00:09 +0200

    edit the code quality

--- main.py
+++ main.py
@@ -15,9 +15,6 @@ from operations import (
 def main():
     tasks = load_from_file("tasks.json")
     print(tasks)
-    global task_id
-    status_list = ["Pinding", "In Progress", "Complete", "Cancelled"]
-    priority_list = [i for i in range(6)]
     task_id = 0
     while True:
         print("________________________Task Menu________________________")
@@ -38,109 +35,59 @@ def main():
             print("invalid input please try again!")
         match option:
             case 1:
-                if not tasks:
-                    task_id = 1
-                else:
-                    task_id = max(int(task_id) for task_id in tasks.keys()) + 1
-                title = input("Enter Task name: ")
-                discription = input("Enter Task discription: ")
-                print("Choose status: ")
-                print("1- Pinding")
-                print("2- In Progress")
-                print("3- Complete")
-                print("4- Cancelled")
-
-                try:
-                    status_option = int(input("Enter the status number:"))
-                    if not status_option >= 1 and not status_option <= 4:
-                        print("Please enter a number between 1 and 4 ")
-                except ValueError:
-                    print("invalid input please try again!")
-
-                if status_option == 1:
-                    status = "Pinding"
-                elif status_option == 2:
-                    status = "In Progress"
-                elif status_option == 3:
-                    status = "Complete"
-                elif status_option == 4:
-                    status = "Cancelled"
-                else:
-                    print(
-                        "Task {} can't be added "
-                        "the status is out of range".format(title)
-                    )
-                    continue
-                try:
-                    priority = int(input("Enter priority from 0 to 5: "))
-                except ValueError:
-                    print("invalid input please try again!")
-                if status in status_list and priority in priority_list:
-                    tasks = add_task(
-                        tasks, task_id, title, discription, status, priority
-                    )
-                    print("Task {} added successfully".format(title))
-                else:
-                    print("Can't be added the priority/status is out of range")
+                tasks = add_task(tasks)
             case 2:
                 display_tasks(tasks)
-                print("Tasks displayed successfully")
             case 3:
                 sort_tasks(tasks)
-                print("According to the priority tasks sorted successfully")
             case 4:
-                print("Mark Task as done according to:")
-                print("1- Task id")
-                print("2- Task name")
-                try:
-                    mark_done_option = int(input("Enter your option: "))
-                    if not mark_done_option >= 1 and not mark_done_option <= 2:
-                        print("Please enter a number between 1 and 2 ")
-                except ValueError:
-                    print("invalid input please try again!")
-                if mark_done_option == 1:
-                    id = int(input("Enter Task id: "))
-                    mark_done_by_id(tasks, id)
-                elif mark_done_option == 2:
-                    title = input("Enter Task name: ")
-                    mark_done_by_name(tasks, title)
+                while True:
+                    print("Mark Task as done according to:")
+                    print("1- Task id")
+                    print("2- Task name")
+                    try:
+                        mark_done_option = int(input("Enter your option: "))
+                        if not mark_done_option == 1 and not mark_done_option == 2:
+                            print("Please enter a number between 1 and 2 ")
+                            print("--------------------------------------")
+                        elif mark_done_option == 1:
+                            mark_done_by_id(tasks)
+                            break
+                        elif mark_done_option == 2:
+                            mark_done_by_name(tasks)
+                            break
+                    except ValueError:
+                        print("invalid input please try again!")
+                        print("--------------------------------------")
             case 5:
-                print("Change priority according to:")
-                print("1- Task id")
-                print("2- Task name")
-                try:
-                    change_priority_option = int(input("Enter your option: "))
-                    if (
-                        not change_priority_option >= 1
-                        and not change_priority_option <= 2
-                    ):
-                        print("Please enter a number between 1 and 2 ")
-                except ValueError:
-                    print("invalid input please try again!")
-                if change_priority_option == 1:
-                    task_id = int(input("Enter Task id: "))
-                    new_priority = int(input("Enter the new priority: "))
-                    if new_priority in priority_list:
-                        change_priority_by_id(tasks, task_id, new_priority)
-                    else:
-                        print("Task {} can't be updated".format(title))
-                        print("because the new priorty is out of range")
-                        continue
-                elif change_priority_option == 2:
-                    title = input("Enter Task name: ")
-                    new_priority = int(input("Enter the new priority: "))
-                    if new_priority in priority_list:
-                        change_priority_by_name(tasks, title, new_priority)
-                    else:
-                        print("Task {} can't be updated".format(title))
-                        print("because the new priorty is out of range")
-                        continue
+                while True:
+                    print("Change priority according to:")
+                    print("1- Task id")
+                    print("2- Task name")
+                    try:
+                        change_priority_option = int(input("Enter your option: "))
+                        if not change_priority_option == 1 and not change_priority_option == 2:
+                            print("Please enter a number between 1 and 2 ")
+                            print("--------------------------------------")
+                        elif change_priority_option == 1:
+                            change_priority_by_id(tasks)
+                            break
+                        elif change_priority_option == 2:
+                            change_priority_by_name(tasks)
+                            break
+                    except ValueError:
+                        print("invalid input please try again!")
+                        print("--------------------------------------")
             case 6:
-                try:
-                    task_id = int(input("Enter Task id: "))
-                except ValueError:
-                    print("invalid input please try again!")
-                delete_task(tasks, task_id)
+                for i in range (3):
+                    try:
+                        task_id = int(input("Enter Task id: "))
+                        delete_task(tasks, task_id)
+                        break
+                    except ValueError:
+                        if i == 2:
+                            print("Invalid !")
+                        print(f"Invalid input please try again! (you have {2-i} attempts)")
             case 7:
                 return
             case 8:
--- operations.py
+++ operations.py
@@ -2,6 +2,10 @@ import json
 import os
 
 
+status_list = ["Pinding", "In Progress", "Complete", "Cancelled"]
+priority_list = [i for i in range(6)]
+
+
 def load_from_file(filename):
     if not os.path.exists(filename):
         return {}
@@ -13,19 +17,46 @@ def load_from_file(filename):
     return tasks
 
 
-def add_task(tasks, task_id, title, description, status="Pending", priority=0):
-    tasks[task_id] = {
-        "title": title,
-        "description": description,
-        "status": status,
-        "priority": priority,
-    }
+def add_task(tasks):
+    if not tasks:
+        task_id = 1
+    else:
+        task_id = max(int(task_id) for task_id in tasks.keys()) + 1
+    title = input("Enter Task name: ")
+    description = input("Enter Task discription: ")
+    print("Choose status: ")
+    for index , status in enumerate(status_list):
+        print(f"{index + 1} - {status}")
+    while True:
+        try:
+            status_option = int(input("Enter the status number:"))
+            status = status_list[status_option - 1]
+            break
+        except ValueError:
+            print("invalid input please try again!")
+        except IndexError:
+            print(f"Please enter a number between 1 and {len(status_list)}")
+    try:
+        priority = int(input("Enter priority from 0 to 5: "))
+    except ValueError:
+        print("invalid input please try again!")
+    if status in status_list and priority in priority_list:
+        tasks[task_id] = {
+            "title": title,
+            "description": description,
+            "status": status,
+            "priority": priority,
+        }
+        print("Task {} added successfully".format(title))
+    else:
+        print("Can't be added the priority/status is out of range")
     return tasks
 
 
 def display_tasks(tasks):
     print("All tasks: ")
     print(tasks)
+    print("Tasks displayed successfully")
 
 
 def sort_tasks(tasks):
@@ -35,9 +66,11 @@ def sort_tasks(tasks):
         )
     )
     print(sorted_tasks)
+    print("According to the priority tasks sorted successfully")
 
 
-def mark_done_by_id(tasks, task_id):
+def mark_done_by_id(tasks):
+    task_id = int(input("Enter Task id: "))
     marked = False
     for key in tasks:
         if int(key) == int(task_id):
@@ -49,7 +82,8 @@ def mark_done_by_id(tasks, task_id):
         print("Task id doesn't exist")
 
 
-def mark_done_by_name(tasks, task_title):
+def mark_done_by_name(tasks):
+    task_title = input("Enter Task name: ")
     marked = False
     for key in tasks:
         if tasks[key]["title"] == task_title:
@@ -61,28 +95,40 @@ def mark_done_by_name(tasks, task_title):
         print("Task title doesn't exist")
 
 
-def change_priority_by_id(tasks, task_id, new_priority):
-    marked = False
-    for key in tasks:
-        if int(key) == int(task_id):
-            tasks[key]["priority"] = new_priority
-            marked = True
-            print("Task {} priority changed successfully".format(task_id))
-            break
-    if not marked:
-        print("Task id doesn't exist")
+def change_priority_by_id(tasks):
+    task_id = int(input("Enter Task id: "))
+    new_priority = int(input("Enter the new priority: "))
+    if new_priority in priority_list:
+        marked = False
+        for key in tasks:
+            if int(key) == int(task_id):
+                tasks[key]["priority"] = new_priority
+                marked = True
+                print("Task {} priority changed successfully".format(task_id))
+                break
+        if not marked:
+            print("Task id doesn't exist")
+    else:
+        print("Task {} can't be updated".format(task_id))
+        print("because the new priorty is out of range")
 
 
-def change_priority_by_name(tasks, task_title, new_priority):
-    marked = False
-    for key in tasks:
-        if tasks[key]["title"] == task_title:
-            tasks[key]["priority"] = new_priority
-            marked = True
-            print("Task {} priority changed successfully".format(task_title))
-            break
-    if not marked:
-        print("Task title doesn't exist")
+def change_priority_by_name(tasks):
+    task_title = input("Enter Task name: ")
+    new_priority = int(input("Enter the new priority: "))
+    if new_priority in priority_list:
+        marked = False
+        for key in tasks:
+            if tasks[key]["title"] == task_title:
+                tasks[key]["priority"] = new_priority
+                marked = True
+                print("Task {} priority changed successfully".format(task_title))
+                break
+        if not marked:
+            print("Task title doesn't exist")
+    else:
+        print("Task {} can't be updated".format(task_title))
+        print("because the new priorty is out of range")
 
 
 def delete_task(tasks, task_id):
