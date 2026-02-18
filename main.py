e3496d3c0b32ca218c1df9cc139d0d0fccc9d519
RaheeqQ <qassrawiraheeq@gmail.com>
2026-02-18 12:42:03 +0200

    import handle exception and edit the code quality in change priority and mark as done functions

--- main.py
+++ main.py
@@ -3,18 +3,21 @@ from operations import (
     add_task,
     display_tasks,
     sort_tasks,
-    mark_done_by_id,
-    mark_done_by_name,
-    change_priority_by_id,
-    change_priority_by_name,
+    mark_as_done,
+    change_priority,
     delete_task,
     save_in_file,
 )
 
 
+from handle_exception import (
+    handle_inner_option,
+    handle_inputs,
+)
+
+
 def main():
     tasks = load_from_file("tasks.json")
-    task_id = 0
     while True:
         print("________________________Task Menu________________________")
         print("choose one from Task menu:")
@@ -26,12 +29,9 @@ def main():
         print("6- Delete Task")
         print("7- Exit Program")
         print("8- Save")
-        try:
-            option = int(input("Enter your option number: "))
-            if not option >= 1 and not option <= 8:
-                print("Please enter a number between 1 and 8 ")
-        except ValueError:
-            print("invalid input please try again!")
+        option = handle_inputs("Enter your option number: ")
+        if option == None:
+            return
         match option:
             case 1:
                 tasks = add_task(tasks)
@@ -40,60 +40,21 @@ def main():
             case 3:
                 sort_tasks(tasks)
             case 4:
-                while True:
-                    print("Mark Task as done according to:")
-                    print("1- Task id")
-                    print("2- Task name")
-                    try:
-                        mark_done_option = int(input("Enter your option: "))
-                        if not mark_done_option == 1 and not mark_done_option == 2:
-                            print("Please enter a number between 1 and 2 ")
-                            print("--------------------------------------")
-                        elif mark_done_option == 1:
-                            mark_done_by_id(tasks)
-                            break
-                        elif mark_done_option == 2:
-                            mark_done_by_name(tasks)
-                            break
-                    except ValueError:
-                        print("invalid input please try again!")
-                        print("--------------------------------------")
+                mark_done_option = handle_inner_option()
+                if not mark_done_option == 3:
+                    mark_as_done(tasks, mark_done_option)
             case 5:
-                while True:
-                    print("Change priority according to:")
-                    print("1- Task id")
-                    print("2- Task name")
-                    try:
-                        change_priority_option = int(input("Enter your option: "))
-                        if not change_priority_option == 1 and not change_priority_option == 2:
-                            print("Please enter a number between 1 and 2 ")
-                            print("--------------------------------------")
-                        elif change_priority_option == 1:
-                            change_priority_by_id(tasks)
-                            break
-                        elif change_priority_option == 2:
-                            change_priority_by_name(tasks)
-                            break
-                    except ValueError:
-                        print("invalid input please try again!")
-                        print("--------------------------------------")
+                change_priority_option = handle_inner_option()
+                if not change_priority_option == 3:
+                    change_priority(tasks, change_priority_option)
             case 6:
-                for i in range (3):
-                    try:
-                        task_id = int(input("Enter Task id: "))
-                        delete_task(tasks, task_id)
-                        break
-                    except ValueError:
-                        if i == 2:
-                            print("Invalid !")
-                        print(f"Invalid input please try again! (you have {2-i} attempts)")
+                delete_task(tasks)
             case 7:
                 return
             case 8:
                 save_in_file(tasks)
-                print("Tasks saved in tasks file successfully")
             case _:
-                return "Invalid option number"
+                print(f"Please enter a number between 1 and 8")
 
 
 if __name__ == "__main__":
--- operations.py
+++ operations.py
@@ -1,5 +1,6 @@
 import json
 import os
+from handle_exception import (handle_inputs)
 
 
 status_list = ["Pending", "In Progress", "Complete", "Cancelled"]
@@ -12,6 +13,7 @@ def load_from_file(filename):
     with open(filename, "r") as file:
         try:
             tasks = json.load(file)
+            tasks = {int(k): v for k, v in tasks.items()}
         except json.JSONDecodeError:
             tasks = {}
     return tasks
@@ -22,14 +24,23 @@ def add_task(tasks):
         task_id = 1
     else:
         task_id = max(int(task_id) for task_id in tasks.keys()) + 1
-    title = input("Enter Task name: ")
-    description = input("Enter Task discription: ")
+    title = input("Enter Task name: ").strip()
+    while not title:
+        print("Task name can't be empty!")
+        title = input("Enter Task name: ").strip()
+    description = input("Enter Task discription: ").strip()
+    while not description:
+        print("Task description can't be empty!")
+        description = input("Enter Task description: ").strip()
     print("Choose status: ")
     for index , status in enumerate(status_list):
         print(f"{index + 1} - {status}")
     while True:
         try:
-            status_option = int(input("Enter the status number:"))
+            status_option = handle_inputs("Enter the status number:")
+            if status_option == None:
+                print("Back to menu")
+                return tasks
             status = status_list[status_option - 1]
             break
         except ValueError:
@@ -37,7 +48,10 @@ def add_task(tasks):
         except IndexError:
             print(f"Please enter a number between 1 and {len(status_list)}")
     try:
-        priority = int(input("Enter priority from 0 to 5: "))
+        priority = handle_inputs("Enter priority from 0 to 5: ")
+        if status_option == None:
+            print("Back to menu")
+            return tasks
     except ValueError:
         print("invalid input please try again!")
     if status in status_list and priority in priority_list:
@@ -65,85 +79,64 @@ def sort_tasks(tasks):
             tasks.items(), key=lambda item: item[1]["priority"]
         )
     )
-    print(sorted_tasks)
+    print(json.dumps(sorted_tasks, indent=4))
     print("According to the priority tasks sorted successfully")
 
 
-def mark_done_by_id(tasks):
-    task_id = int(input("Enter Task id: "))
-    marked = False
-    for key in tasks:
-        if int(key) == int(task_id):
-            tasks[key]["status"] = "Complete"
-            marked = True
-            print("Task {} marked as done successfully".format(task_id))
-            break
-    if not marked:
-        print("Task id doesn't exist")
-
-
-def mark_done_by_name(tasks):
-    task_title = input("Enter Task name: ")
-    marked = False
-    for key in tasks:
-        if tasks[key]["title"] == task_title:
-            tasks[key]["status"] = "Complete"
-            marked = True
-            print("Task {} marked as done successfully".format(task_title))
-            break
-    if not marked:
-        print("Task title doesn't exist")
-
-
-def change_priority_by_id(tasks):
-    task_id = int(input("Enter Task id: "))
-    new_priority = int(input("Enter the new priority: "))
-    if new_priority in priority_list:
-        marked = False
-        for key in tasks:
-            if int(key) == int(task_id):
-                tasks[key]["priority"] = new_priority
-                marked = True
-                print("Task {} priority changed successfully".format(task_id))
+def mark_as_done(tasks, option):
+    task = None
+    if option == 1:
+        task_id = handle_inputs("Enter Task id: ")
+        task = tasks.get(task_id)
+    elif option == 2:
+        task_title = input("Enter Task name: ")
+        for t in tasks.values():
+            if t["title"] == task_title:
+                task = t
                 break
-        if not marked:
-            print("Task id doesn't exist")
+    if task:
+        task["status"] = "Complete"
+        print("Task marked as done successfully")
     else:
-        print("Task {} can't be updated".format(task_id))
+        print("Task doesn't exist")
+
+
+def change_priority(tasks, option):
+    new_priority = handle_inputs("Enter the new priority: ")
+    if new_priority not in priority_list:
+        print("Task can't be updated")
         print("because the new priorty is out of range")
-
-
-def change_priority_by_name(tasks):
-    task_title = input("Enter Task name: ")
-    new_priority = int(input("Enter the new priority: "))
-    if new_priority in priority_list:
-        marked = False
-        for key in tasks:
-            if tasks[key]["title"] == task_title:
-                tasks[key]["priority"] = new_priority
-                marked = True
-                print("Task {} priority changed successfully".format(task_title))
-                break
-        if not marked:
-            print("Task title doesn't exist")
+        return
+    task = None
+    if option == 1:
+        task_id = handle_inputs("Enter Task id: ")
+        task = tasks.get(task_id)
+    elif option == 2:
+        task_title = input("Enter Task name: ")
+        for t in tasks.values():
+            if t["title"] == task_title:
+                task = t
+                break      
+    if task:
+        task["priority"] = new_priority
+        print("Task priority changed successfully")
     else:
-        print("Task {} can't be updated".format(task_title))
-        print("because the new priorty is out of range")
+        print("Task doesn't exist")
 
 
-def delete_task(tasks, task_id):
-    deleted = False
-    for key in tasks:
-        if int(key) == int(task_id):
-            val = tasks.pop(key)
-            print("Value popped: ", val)
-            deleted = True
-            print("Task {} deleted successfully".format(task_id))
-            break
-    if not deleted:
-        print("Task title doesn't exist")
+def delete_task(tasks):
+    task_id = handle_inputs("Enter Task id: ")
+    task = tasks.get(task_id)
+    if task:
+        val = tasks.pop(task_id)
+        print("Task popped: ", val)
+        print("Task {} deleted successfully".format(task_id))
+    else:
+        print("Task doesn't exist")
+
 
 
 def save_in_file(tasks):
     with open("tasks.json", "w") as file:
         json.dump(tasks, file, indent=4)
+    print("Tasks saved in tasks file successfully")
